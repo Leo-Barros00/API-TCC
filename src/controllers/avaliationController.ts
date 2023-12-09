80b65089930b667e42741e40eae6fd334e389155
Filipe Zanelato <filipe_zanelato@hotmail.com>
2023-12-09 17:19:49 -0300

    fix: syntax errors

--- src/controllers/avaliationController.ts
+++ src/controllers/avaliationController.ts
@@ -2,20 +2,19 @@ import { NextFunction, Request, Response } from 'express'
 import Controller from '../decorators/Controller'
 import { AuthContext, Get, Post } from '../decorators/handlerDecorator'
 import AvaliationService from '../services/avaliationService'
-import 'reflect-metadata';
-
+import 'reflect-metadata'
 
 @Controller('/avaliation')
 class AvaliationController {
   @Post('/send')
   public async sendNewAvaliation(req: Request, res: Response, next: NextFunction) {
     try {
-      const { value, description } = req.body
+      const { value, description, userId } = req.body
 
       const avaliationModel = {
-        userId: res.locals.userId,
+        userId,
         number: value,
-        description
+        description,
       }
 
       await AvaliationService.sendNewAvaliation(avaliationModel)
@@ -25,35 +24,35 @@ class AvaliationController {
     }
   }
 
-  @Get('/:userId', AuthContext.Unprotected)
+  @Get('/:userId')
   public async getAvaliation(req: Request, res: Response, next: NextFunction) {
     try {
       const { userId } = req.params
 
-      const avaliations = await AvaliationService.getAvaliation(userId)       
-      
-      const media = avaliations.reduce((acc, avaliation) => {
-        return acc + Number(avaliation.number);
-      }, 0) / avaliations.length; 
-      console.log(media);
-      res.status(200).send({media})
-    } catch (error) {
-      next(error)
-    }
-  }  
-  
-  @Get('/', AuthContext.Unprotected)
-  public async getAllAvaliation(req: Request, res: Response, next: NextFunction) {
-    try {
-      const { userId } = req.params
+      const avaliations = await AvaliationService.getAvaliation(userId)
 
-      const avaliations = await AvaliationService.getAllAvaliation()
-      res.sendStatus(201)
+      const media =
+        avaliations.reduce((acc, avaliation) => {
+          return acc + Number(avaliation.number)
+        }, 0) / avaliations.length
+
+      res.status(200).send({ media })
     } catch (error) {
       next(error)
     }
   }
 
+  @Get('/')
+  public async getAllAvaliation(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { userId } = req.params
+
+      const avaliations = await AvaliationService.getAllAvaliation()
+      res.status(200).send({ avaliations })
+    } catch (error) {
+      next(error)
+    }
+  }
 }
 
 export default AvaliationController
